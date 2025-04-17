50eda2e518f3bead0da3f9895bda9d16b17cae78
sootopia <shpark7502@naver.com>
2025-04-18 00:23:12 +0900

    feat: 헤더 컴포넌트 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="absolute top-0 left-0 right-0 h-[60px] border-b border-solid border-slate-200">
+      <div className="container px-5 h-full flex items-center">
+        <h1 className="text-2xl font-bold">출퇴근 교통정보</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
